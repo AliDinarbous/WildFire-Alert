015f0dacc41c8b53fdcfcd95c2ea597fd40af2a0
dinarbous <alidinarbous04@gmail.com>
2026-04-01 20:29:05 +0200

    feat: authentification(login/register)

---
+++ backend/app/controllers/auth.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from database import get_db
+from security import verify_password, create_access_token, get_password_hash
+import crud
+import schemas
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+# New User subcription
+@router.post("/register", response_model=schemas.UserResponse)
+def register(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    if crud.get_user_by_username(db, username=user_data.username):
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    if crud.get_user_by_email(db, email=user_data.email):
+        raise HTTPException(status_code=400, detail="Email already exists")
+    
+    hashed = get_password_hash(user_data.password)
+    return crud.create_user(db, user_data, hashed)
+
+
+
+
+
+# Se connecter
+@router.post("/login", response_model=schemas.Token)
+def login(user_data: schemas.UserLogin, db: Session = Depends(get_db)):
+
+  user = crud.get_user_by_username(db, username=user_data.username)
+
+  if not user:
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Invalid credentials"
+    )
+
+  if not verify_password(user_data.password, user.hashed_password):
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Invalid credentials"
+    )
+
+  token = create_access_token({"sub": user.username})
+
+  return {
+    "access_token": token,
+    "token_type": "bearer"
+  }
