2626b16f304281a73e9757e1ea116cd1d3a98d91
AliDinarbous <ali.dinarbous58@gmail.com>
2026-04-02 19:31:24 +0200

    chore: modif des fichiers docker et auth

--- backend/app/controllers/auth.py
+++ backend/app/controllers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordRequestForm
 from database import get_db
 from security import verify_password, create_access_token, get_password_hash
 import crud
@@ -24,28 +24,12 @@ def register(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 
-
-# Se connecter
 @router.post("/login", response_model=schemas.Token)
-def login(user_data: schemas.UserLogin, db: Session = Depends(get_db)):
-
-  user = crud.get_user_by_username(db, username=user_data.username)
-
-  if not user:
-    raise HTTPException(
-      status_code=status.HTTP_401_UNAUTHORIZED,
-      detail="Invalid credentials"
-    )
-
-  if not verify_password(user_data.password, user.hashed_password):
-    raise HTTPException(
-      status_code=status.HTTP_401_UNAUTHORIZED,
-      detail="Invalid credentials"
-    )
-
-  token = create_access_token({"sub": user.username})
-
-  return {
-    "access_token": token,
-    "token_type": "bearer"
-  }
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = crud.get_user_by_username(db, username=form_data.username)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    if not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token = create_access_token({"sub": user.username})
+    return {"access_token": token, "token_type": "bearer"}
